084a4aa8ec142588a3686e9db4806d369f224f62
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-07-21 19:20:36 -0500

    package.json fix

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+// Depending on the current path, component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
     <nav className="navbar navbar-light bg-danger">
